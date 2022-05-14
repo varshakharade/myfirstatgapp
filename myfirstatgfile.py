f7cacb013d501a718268333660bfcd6ba67cbee4
varshakharade <105452845+varshakharade@users.noreply.github.com>
2022-05-14 16:32:16 +0530

    Update myfirstatgfile.py
    fixed

--- myfirstatgfile.py
+++ myfirstatgfile.py
@@ -1,2 +1,6 @@
 import streamlit
-streamlit.title('Hello')
+streamlit.title('Healthy Dinner')
+streamlit.header('Breakfast Menu')
+streamlit.text('Boiled Eggs')
+streamlit.text('Bread & Peanutbutter')
+streamlit.text('Milk')
