2d23cdb44ce5c9850979c920929d95270ea52ef0
varshakharade <105452845+varshakharade@users.noreply.github.com>
2022-05-22 16:28:41 +0530

    Update myfirstatgfile.py

--- myfirstatgfile.py
+++ myfirstatgfile.py
@@ -27,5 +27,5 @@ import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" +fruit_choice)
 
 
-#fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-#streamlit.dataframe(fruityvice_normalized)
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+streamlit.dataframe(fruityvice_normalized)
