4ee78eab011068a903ad2210d40e2ebcb30ff61d
varshakharade <105452845+varshakharade@users.noreply.github.com>
2022-05-23 23:15:01 +0530

    Update myfirstatgfile.py

--- myfirstatgfile.py
+++ myfirstatgfile.py
@@ -31,10 +31,9 @@ fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 streamlit.dataframe(fruityvice_normalized)
 
 import snowflake.connector
-
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
-my_cur.execute("SELECT * from fruit_load_list")
+my_cur.execute("SELECT * FROM fruit_load_list")
 my_data_row = my_cur.fetchone()
-streamlit.text("The fruit load list contains:")
-streamlit.text(my_data_row)
+streamlit.header("The fruit load list contains:")
+streamlit.dataframe(my_data_row)
