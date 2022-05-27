50227ab58043a1fe7f3f85da14107821c6fcf54c
varshakharade <105452845+varshakharade@users.noreply.github.com>
2022-05-27 16:42:15 +0530

    Update myfirstatgfile.py

--- myfirstatgfile.py
+++ myfirstatgfile.py
@@ -1,4 +1,9 @@
 import streamlit
+import pandas
+import requests
+import snowflake.connector
+from urllib.error import URLError
+
 streamlit.title('Healthy Dinner')
 streamlit.header('Breakfast Menu')
 streamlit.text('Boiled Eggs')
@@ -7,7 +12,7 @@ streamlit.text('🥑🍞Milk')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
-import pandas
+#import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
 #streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Apple','Avocado'])
@@ -23,11 +28,12 @@ streamlit.header('Fruityvice Fruit Advice!')
 fruit_choice = streamlit.text_input('What fruit would you like information about?', 'apple')
 streamlit.write('The user entered', fruit_choice)
 
-import requests
+#import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 streamlit.dataframe(fruityvice_normalized)
+streamlit.stop()
 
 import snowflake.connector
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
