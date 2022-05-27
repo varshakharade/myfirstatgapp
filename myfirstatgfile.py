0e665b3e873d4c2860d3d011d7c81151fe341808
varshakharade <105452845+varshakharade@users.noreply.github.com>
2022-05-27 16:25:13 +0530

    Update myfirstatgfile.py

--- myfirstatgfile.py
+++ myfirstatgfile.py
@@ -39,4 +39,4 @@ streamlit.dataframe(my_data_rows)
 
 add_my_fruit = streamlit.text_input('What fruit would you like to add?', 'jackfruit')
 streamlit.write('Thanks for adding jackfruit', add_my_fruit)
-
+my_cur.execute("insert into fruit_load_list values ('from streamlit')")
