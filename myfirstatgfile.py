3c55c50c9e669f687fb3edaeb0b9d67bcfb01f89
varshakharade <105452845+varshakharade@users.noreply.github.com>
2022-05-27 15:33:15 +0530

    Update myfirstatgfile.py

--- myfirstatgfile.py
+++ myfirstatgfile.py
@@ -24,6 +24,9 @@ fruit_choice = streamlit.text_input('What fruit would you like information about
 streamlit.write('The user entered', fruit_choice)
 
 import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" +fruit_choice)
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
+
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+streamlit.dataframe(fruityvice_normalized)
 
 
